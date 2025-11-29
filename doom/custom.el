e8911d9ff245b169e5f3faa68d2a94447e15434a
wyvernbw <iamwyvern511@gmail.com>
2025-11-29 16:16:23 +0200

    add doom emacs config from mac

---
+++ doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/windlight-animations/tasks.org"
+     "/Users/calin/org/balada-lui-nicu-voinicu.org"
+     "/Users/calin/org/nietzsche.org" "/Users/calin/org/tasks.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
